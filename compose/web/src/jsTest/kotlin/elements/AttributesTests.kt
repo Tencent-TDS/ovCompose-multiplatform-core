22cd3ae5017ba3cac50f232fe3eccf8c7c22d8c4
Oleksandr Karpovich <oleksandr.karpovich@jetbrains.com>
2021-05-06 14:07:29 +0200

    [:compose:web] Make 'disabled' attribute of Button applied only when it's `true`
    Change-Id: If4df0cf91a3562d7865051edce11328757feecd7

--- compose/web/src/jsTest/kotlin/elements/AttributesTests.kt
+++ compose/web/src/jsTest/kotlin/elements/AttributesTests.kt
@@ -16,9 +16,15 @@
 
 package elements
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.web.attributes.disabled
 import androidx.compose.web.attributes.forId
+import androidx.compose.web.elements.Button
 import androidx.compose.web.elements.Label
 import androidx.compose.web.elements.Text
+import org.w3c.dom.HTMLButtonElement
 import runTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -68,5 +74,26 @@ class AttributesTests {
             expected = "<label for=\"lb1\" style=\"\">label</label>",
             actual = root.innerHTML
         )
+    }
+
+    @Test
+    fun buttonDisabledAttributeAddedOnlyWhenTrue() = runTest {
+        var disabled by mutableStateOf(false)
+
+        composition {
+            Button(
+                attrs = {
+                    disabled(disabled)
+                }
+            ) {}
+        }
+
+        val btn = root.firstChild as HTMLButtonElement
+        assertEquals(null, btn.getAttribute("disabled"))
+
+        disabled = true
+        waitChanges()
+
+        assertEquals("", btn.getAttribute("disabled"))
     }
 }
