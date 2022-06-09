e5974d801bbf69e952f2c0a924bf486a416d58e3
Igor Demin <igordmn@gmail.com>
2022-06-09 17:22:33 +0300

    Disable tests for macOs

--- compose/ui/ui/src/desktopTest/kotlin/androidx/compose/ui/awt/ComposeFocusTest.kt
+++ compose/ui/ui/src/desktopTest/kotlin/androidx/compose/ui/awt/ComposeFocusTest.kt
@@ -42,6 +42,7 @@ import javax.swing.JFrame
 import kotlin.random.Random
 import kotlinx.coroutines.runBlocking
 import org.jetbrains.skiko.MainUIDispatcher
+import org.jetbrains.skiko.hostOs
 import org.junit.Assume
 import org.junit.Test
 import org.junit.experimental.categories.Categories
@@ -425,6 +426,8 @@ class ComposeFocusTest {
 
 fun runFocusTest(action: suspend FocusTestScope.() -> Unit) {
     Assume.assumeFalse(GraphicsEnvironment.getLocalGraphicsEnvironment().isHeadlessInstance)
+    // on macOs if we run all tests, the window starts unfocused, so tests fail
+    Assume.assumeFalse(hostOs.isMacOS)
     runBlocking(MainUIDispatcher) {
         val scope = FocusTestScope()
         try {
