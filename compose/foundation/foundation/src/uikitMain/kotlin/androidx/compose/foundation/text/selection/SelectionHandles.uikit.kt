80c9b8bc7094da2482c2d2359721a168b26c644b
dima.avdeev <dima.avdeev@jetbrains.com>
2022-09-19 11:29:53 +0400

    SelectionHandles.uikit.kt

--- compose/foundation/foundation/src/uikitMain/kotlin/androidx/compose/foundation/text/selection/SelectionHandles.uikit.kt
+++ compose/foundation/foundation/src/uikitMain/kotlin/androidx/compose/foundation/text/selection/SelectionHandles.uikit.kt
@@ -18,26 +18,15 @@ package androidx.compose.foundation.text.selection
 
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.text.Handle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
-import androidx.compose.ui.draw.CacheDrawScope
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.BlendMode
-import androidx.compose.ui.graphics.Canvas
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.ImageBitmapConfig
-import androidx.compose.ui.graphics.drawscope.CanvasDrawScope
-import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.ResolvedTextDirection
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntRect
@@ -88,19 +77,18 @@ internal actual fun SelectionHandle(
         Spacer(
             modifier.size((PADDING + RADIUS) * 2.dp)
                 .drawWithCache {
-                    val handleImage = createHandleImage(lineHeight, isLeft)
-                    val colorFilter = ColorFilter.tint(handleColor)
                     onDrawWithContent {
                         drawContent()
-                        drawImage(
-                            image = handleImage,
-                            colorFilter = colorFilter
+                        drawCircle(
+                            color = handleColor,
+                            radius = RADIUS,
+                            center = center
                         )
                     }
                 }
                 .drawBehind {
                     drawRect(
-                        handleColor,
+                        color = handleColor,
                         topLeft = Offset(
                             x = PADDING + RADIUS - THICKNESS / 2,
                             y = if (isLeft) PADDING + RADIUS else PADDING - lineHeight
@@ -110,78 +98,6 @@ internal actual fun SelectionHandle(
                 }
         )
     }
-}
-
-/**
- * The cache for the image mask created to draw selection/cursor handle, so that we don't need to
- * recreate them.
- */
-private object HandleImageCache {
-    var imageBitmap: ImageBitmap? = null
-    var canvas: Canvas? = null
-    var canvasDrawScope: CanvasDrawScope? = null
-}
-
-/**
- * Create an image bitmap for the basic shape of a selection handle or cursor handle. It is an
- * circle with a rectangle covering its left top part.
- *
- * To draw the right selection handle, directly draw this image bitmap.
- * To draw the left selection handle, mirror the canvas first and then draw this image bitmap.
- * To draw the cursor handle, translate and rotated the canvas 45 degrees, then draw this image
- * bitmap.
- *
- * @param lineHeight the lineHeight of text line in selection/cursor handle.
- * CanvasDrawScope objects so that we only recreate them when necessary.
- */
-internal fun CacheDrawScope.createHandleImage(lineHeight: Float, isLeft: Boolean): ImageBitmap {
-    // The edge length of the square bounding box of the selection/cursor handle. This is also
-    // the size of the bitmap needed for the bitmap mask.
-
-    var imageBitmap = HandleImageCache.imageBitmap
-    var canvas = HandleImageCache.canvas
-    var drawScope = HandleImageCache.canvasDrawScope
-
-    // If the cached bitmap is null or too small, we need to create new bitmap.
-    if (
-        imageBitmap == null ||
-        canvas == null
-    ) {
-        imageBitmap = ImageBitmap(
-            width = (PADDING * 2 + RADIUS * 2).toInt(),
-            height = (PADDING * 2 + RADIUS * 2).toInt(),
-            config = ImageBitmapConfig.Alpha8
-        )
-        HandleImageCache.imageBitmap = imageBitmap
-        canvas = Canvas(imageBitmap)
-        HandleImageCache.canvas = canvas
-    }
-    if (drawScope == null) {
-        drawScope = CanvasDrawScope()
-        HandleImageCache.canvasDrawScope = drawScope
-    }
-
-    drawScope.draw(
-        this,
-        layoutDirection,
-        canvas,
-        Size(imageBitmap.width.toFloat(), imageBitmap.height.toFloat())
-    ) {
-        // Clear the previously rendered portion within this ImageBitmap as we could
-        // be re-using it
-        drawRect(
-            color = Color.Black,
-            size = size,
-            blendMode = BlendMode.Clear
-        )
-        // Draw the circle
-        drawCircle(
-            color = Color(0xFF000000),
-            radius = RADIUS,
-            center = Offset(PADDING + RADIUS, PADDING + RADIUS)
-        )
-    }
-    return imageBitmap
 }
 
 @Composable
@@ -206,6 +122,16 @@ internal fun HandlePopup(
         popupPositionProvider = popupPositioner,
         content = content
     )
+}
+
+/**
+ * Transform value:T to State<T>
+ */
+@Composable
+private inline fun <reified T> valueToState(value: T): State<T> {
+    val state = remember { mutableStateOf(value) }
+    state.value = value
+    return state
 }
 
 /**
@@ -242,12 +168,3 @@ internal fun isHandleLtrDirection(
         direction == ResolvedTextDirection.Rtl && areHandlesCrossed
 }
 
-/**
- * Transform value:T to State<T>
- */
-@Composable
-private inline fun <reified T> valueToState(value: T): State<T> {
-    val state = remember { mutableStateOf(value) }
-    state.value = value
-    return state
-}
