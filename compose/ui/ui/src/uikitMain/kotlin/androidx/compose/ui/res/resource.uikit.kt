2abf5b778202e86277989611634e3ef0e6116581
dima.avdeev <dima.avdeev@jetbrains.com>
2022-11-17 21:30:03 +0400

    uikit resources in compose/ui/ui

---
+++ compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/res/resource.uikit.kt
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.ui.res
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.ImageBitmap
+
+import platform.Foundation.*
+import kotlinx.cinterop.addressOf
+import kotlinx.cinterop.usePinned
+import platform.Foundation.NSBundle
+import platform.Foundation.NSData
+import platform.posix.memcpy
+import androidx.compose.ui.graphics.toComposeImageBitmap
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.skia.Image
+
+fun interface ByteSource { //todo move to commonMain
+    suspend fun readBytes(): ByteArray //todo in future use streaming
+}
+
+fun interface ResourceProvider<T> { //todo move to commonMain
+    suspend fun provide(): T
+}
+
+@Composable
+fun <T> ResourceProvider<T>.rememberBlocking() = remember { //todo move to commonMain
+    runBlocking {
+        provide()
+    }
+}
+
+@Composable
+fun <T> ResourceProvider<T>.rememberAsync(): State<T?> { //todo move to commonMain
+    val state = remember { mutableStateOf<T?>(null) }
+    LaunchedEffect(Unit) {
+        state.value = provide()
+    }
+    return state
+}
+
+fun ByteSource.asImageBitmap(): ResourceProvider<ImageBitmap> = ResourceProvider {//todo move to skikoMain
+    Image.makeFromEncoded(readBytes()).toComposeImageBitmap()
+}
+
+fun resource(path: String): ByteSource = ByteSource { // todo expect / actual
+    val absolutePath = NSBundle.mainBundle.resourcePath + "/" + path
+    val contentsAtPath: NSData = NSFileManager.defaultManager().contentsAtPath(absolutePath)!!
+    val byteArray = ByteArray(contentsAtPath.length.toInt())
+    byteArray.usePinned {
+        memcpy(it.addressOf(0), contentsAtPath.bytes, contentsAtPath.length)
+    }
+    byteArray
+}
