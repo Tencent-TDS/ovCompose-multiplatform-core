3abb7801ed005f300c424c124a2d2c1c608d3854
dima.avdeev <dima.avdeev@jetbrains.com>
2022-12-04 13:35:08 +0400

    UIKitInterop.uikit.kt

--- compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/interop/UIKitInterop.uikit.kt
+++ compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/interop/UIKitInterop.uikit.kt
@@ -20,7 +20,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.SnapshotStateObserver
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.focusTarget
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
@@ -28,10 +35,18 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntRect
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.round
+import kotlinx.atomicfu.AtomicBoolean
+import kotlinx.atomicfu.atomic
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import platform.CoreGraphics.CGRectMake
 import platform.UIKit.UIColor
 import platform.UIKit.UIView
@@ -43,22 +58,29 @@ import platform.UIKit.setFrame
 import platform.UIKit.setNeedsDisplay
 import platform.UIKit.setNeedsUpdateConstraints
 
+val NoOpUpdate: UIView.() -> Unit = {}
+
+/**
+ * TODO doc
+ */
 @Composable
 public fun <T : UIView> UIKitInteropView(
-    modifier: Modifier = Modifier,
+    background: Color = Color.White,
     factory: () -> T,
-//    background: Color = Color.White,
+    modifier: Modifier = Modifier,
+    update: (T) -> Unit = NoOpUpdate
 ) {
-    val componentInfo = remember { ComponentInfo() }
+    val componentInfo = remember { ComponentInfo<T>() }
 
     val root = LocalLayerContainer.current
     val density = LocalDensity.current.density
-//    val focusSwitcher = remember { FocusSwitcher(componentInfo, focusManager) }
+    val focusManager = LocalFocusManager.current
+    val focusSwitcher = remember { FocusSwitcher(componentInfo, focusManager) }
 
     Box(
         modifier = modifier.onGloballyPositioned { childCoordinates ->
             val coordinates = childCoordinates.parentCoordinates!!
-            val location: IntOffset = coordinates.localToWindow(Offset.Zero).round()
+            val location = coordinates.localToWindow(Offset.Zero).round()
             val size = coordinates.size
             val rect = IntRect(location, size) / density
             componentInfo.container.setFrame(rect.toCGRect())
@@ -68,21 +90,126 @@ public fun <T : UIView> UIKitInteropView(
 //            componentInfo.container.repaint()
         }
     ) {
-//        focusSwitcher.Content()
+        focusSwitcher.Content()
     }
 
     DisposableEffect(factory) {
+//        val focusListener = object : FocusListener {
+//            override fun focusGained(e: FocusEvent) {
+//                if (componentInfo.container.isParentOf(e.oppositeComponent)) {
+//                    when (e.cause) {
+//                        FocusEvent.Cause.TRAVERSAL_FORWARD -> focusSwitcher.moveForward()
+//                        FocusEvent.Cause.TRAVERSAL_BACKWARD -> focusSwitcher.moveBackward()
+//                        else -> Unit
+//                    }
+//                }
+//            }
+//
+//            override fun focusLost(e: FocusEvent) = Unit
+//        }
+//        root.addFocusListener(focusListener)
+        componentInfo.component = factory()
         componentInfo.container = UIView().apply {
 //            layout = BorderLayout(0, 0)
+//            focusTraversalPolicy = object : LayoutFocusTraversalPolicy() {
+//                override fun getComponentAfter(aContainer: Container?, aComponent: Component?): Component? {
+//                    return if (aComponent == getLastComponent(aContainer)) {
+//                        root
+//                    } else {
+//                        super.getComponentAfter(aContainer, aComponent)
+//                    }
+//                }
+//
+//                override fun getComponentBefore(aContainer: Container?, aComponent: Component?): Component? {
+//                    return if (aComponent == getFirstComponent(aContainer)) {
+//                        root
+//                    } else {
+//                        super.getComponentBefore(aContainer, aComponent)
+//                    }
+//                }
+//            }
+//            isFocusCycleRoot = true
+            addSubview(componentInfo.component)
         }
+        componentInfo.updater = Updater(componentInfo.component, update)
         root.addSubview(componentInfo.container)
         onDispose {
             componentInfo.container.removeFromSuperview()
+            componentInfo.updater.dispose()
+//            root.removeFocusListener(focusListener)
         }
     }
 
     SideEffect {
-        componentInfo.container.backgroundColor = UIColor.yellowColor()
+        componentInfo.container.backgroundColor = parseColor(background)
+        componentInfo.updater.update = update
+    }
+}
+
+private class FocusSwitcher<T : UIView>(
+    private val info: ComponentInfo<T>,
+    private val focusManager: FocusManager
+) {
+    private val backwardRequester = FocusRequester()
+    private val forwardRequester = FocusRequester()
+    private var isRequesting = false
+
+    fun moveBackward() {
+        try {
+            isRequesting = true
+            backwardRequester.requestFocus()
+        } finally {
+            isRequesting = false
+        }
+        focusManager.moveFocus(FocusDirection.Previous)
+    }
+
+    fun moveForward() {
+        try {
+            isRequesting = true
+            forwardRequester.requestFocus()
+        } finally {
+            isRequesting = false
+        }
+        focusManager.moveFocus(FocusDirection.Next)
+    }
+
+    @Composable
+    fun Content() {
+        Box(
+            Modifier
+                .focusRequester(backwardRequester)
+                .onFocusChanged {
+                    if (it.isFocused && !isRequesting) {
+                        focusManager.clearFocus(force = true)
+
+//                        val component = info.container.focusTraversalPolicy.getFirstComponent(info.container)
+//                        if (component != null) {
+//                            component.requestFocus(FocusEvent.Cause.TRAVERSAL_FORWARD)
+//                        } else {
+//                            moveForward()
+//                        }
+                    }
+                }
+                .focusTarget()
+        )
+        Box(
+            Modifier
+                .focusRequester(forwardRequester)
+                .onFocusChanged {
+                    if (it.isFocused && !isRequesting) {
+                        focusManager.clearFocus(force = true)
+
+//                        val component = info.container.focusTraversalPolicy.getLastComponent(info.container)
+//                        if (component != null) {
+//                            component.requestFocus(FocusEvent.Cause.TRAVERSAL_BACKWARD)
+//                        } else {
+//                            moveBackward()
+//                        }
+                    }
+                }
+                .focusTarget()
+        )
     }
 }
 
@@ -105,8 +232,72 @@ private fun Box(modifier: Modifier, content: @Composable () -> Unit = {}) {
     )
 }
 
-private class ComponentInfo {
-    lateinit var container: UIView
+private fun parseColor(color: Color): UIColor {
+    return UIColor(
+        red = color.red.toDouble(),
+        green = color.green.toDouble(),
+        blue = color.blue.toDouble(),
+        alpha = color.alpha.toDouble()
+    )
 }
 
+private class ComponentInfo<T : UIView> {
+    lateinit var container: UIView
+    lateinit var component: T
+    lateinit var updater: Updater<T>
+}
 
+private class Updater<T : UIView>(
+    private val component: T,
+    update: (T) -> Unit
+) {
+    private var isDisposed = false
+    private val isUpdateScheduled = atomic(false)
+    private val snapshotObserver = SnapshotStateObserver { command ->
+        command()
+    }
+
+    private val scheduleUpdate = { _: T ->
+        if (!isUpdateScheduled.getAndSet(true)) {
+            GlobalScope.launch {
+                withContext(Dispatchers.Main) {
+                    isUpdateScheduled.value = false
+                    if (!isDisposed) {
+                        performUpdate()
+                    }
+                }
+            }
+            MainScope().launch { // instead of DispatchQueue.main.async {
+                // run on main thread
+
+            }
+        }
+    }
+
+    var update: (T) -> Unit = update
+        set(value) {
+            if (field != value) {
+                field = value
+                performUpdate()
+            }
+        }
+
+    private fun performUpdate() {
+        // don't replace scheduleUpdate by lambda reference,
+        // scheduleUpdate should always be the same instance
+        snapshotObserver.observeReads(component, scheduleUpdate) {
+            update(component)
+        }
+    }
+
+    init {
+        snapshotObserver.start()
+        performUpdate()
+    }
+
+    fun dispose() {
+        snapshotObserver.stop()
+        snapshotObserver.clear()
+        isDisposed = true
+    }
+}
