5a13bda334106cdfb5434041ed8baeb795394b6e
dima.avdeev <dima.avdeev@jetbrains.com>
2023-02-25 00:05:30 +0400

    ComposeUITextField

--- compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/interop/ComposeUITextField.kt
+++ compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/interop/ComposeUITextField.kt
@@ -1,33 +1,17 @@
-/*
- * Copyright 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package androidx.compose.mpp.demo
+package androidx.compose.ui.interop
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.interop.UIKitInteropView
 import kotlinx.cinterop.ObjCAction
 import platform.CoreGraphics.CGRectMake
 import platform.Foundation.NSSelectorFromString
 import platform.UIKit.UIControlEventEditingChanged
 import platform.UIKit.UITextField
 
+//todo in the future, move to separate library
 @Composable
-internal fun ComposeUITextField(modifier: Modifier, value: String, onValueChange: (String) -> Unit) {
+fun ComposeUITextField(modifier: Modifier, value: String, onValueChange: (String) -> Unit) {
     UIKitInteropView(
         background = Color.Green,
         modifier = modifier,
