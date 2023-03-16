26fe119d73596023ee260c90c27628271acb3df3
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2023-03-16 14:55:33 +0100

    Move focusedOwner to private back

--- compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/ComposeScene.skiko.kt
+++ compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/ComposeScene.skiko.kt
@@ -423,7 +423,7 @@ class ComposeScene internal constructor(
         forEachOwner { it.clearInvalidObservations() }
     }
 
-    internal var focusedOwner: SkiaBasedOwner? = null
+    private var focusedOwner: SkiaBasedOwner? = null
     private var pressOwner: SkiaBasedOwner? = null
     private var lastMoveOwner: SkiaBasedOwner? = null
     private fun hoveredOwner(event: PointerInputEvent): SkiaBasedOwner? =
