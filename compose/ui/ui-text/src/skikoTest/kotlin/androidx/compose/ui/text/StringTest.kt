6dde5314875f83f96f42a2ebb9f95dc12fc1cde4
Ivan Matkov <ivan.matkov@jetbrains.com>
2023-04-13 14:04:25 +0200

    Add test for locale-dependent string manupulations

---
+++ compose/ui/ui-text/src/skikoTest/kotlin/androidx/compose/ui/text/StringTest.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.ui.text
+
+import androidx.compose.ui.text.intl.LocaleList
+import kotlin.test.*
+
+class StringTest {
+
+    @Test
+    fun twoCharactersRepresentedAsSingleUnicodeLetter() {
+        // https://en.wikipedia.org/wiki/D%C5%BE
+        val lowercase = "ǆ" // U+01C6
+        val titlecase = "ǅ" // U+01C5
+        val uppercase = "Ǆ" // U+01C4
+        val serbianLocale = LocaleList("sr")
+
+        assertEquals(lowercase.toUpperCase(serbianLocale), uppercase)
+        assertEquals(lowercase.toLowerCase(serbianLocale), lowercase)
+        assertEquals(lowercase.capitalize(serbianLocale), titlecase)
+        assertEquals(lowercase.decapitalize(serbianLocale), lowercase)
+
+        assertEquals(titlecase.toUpperCase(serbianLocale), uppercase)
+        assertEquals(titlecase.toLowerCase(serbianLocale), lowercase)
+        assertEquals(titlecase.capitalize(serbianLocale), titlecase)
+        assertEquals(titlecase.decapitalize(serbianLocale), lowercase)
+
+        assertEquals(uppercase.toUpperCase(serbianLocale), uppercase)
+        assertEquals(uppercase.toLowerCase(serbianLocale), lowercase)
+        assertEquals(uppercase.capitalize(serbianLocale), uppercase)
+        assertEquals(uppercase.decapitalize(serbianLocale), lowercase)
+    }
+}
