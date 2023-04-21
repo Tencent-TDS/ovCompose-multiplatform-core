f2e9bf500d9393ed84b58417e76ca84a019e217b
dima.avdeev <dima.avdeev@jetbrains.com>
2023-04-21 11:23:03 +0400

    Reproduce LazyVerticalStaggeredGrid compile time error

---
+++ compose/mpp/demo/src/commonMain/kotlin/androidx/compose/mpp/demo/ExampleStaggeredGrid.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.mpp.demo
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import kotlin.random.Random
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun ExampleStaggeredGrid() {
+    LazyVerticalStaggeredGrid(
+        StaggeredGridCells.Fixed(3),
+        contentPadding = PaddingValues(8.dp),
+        verticalItemSpacing = 8.dp,
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        items(100) {
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .height(remember { (100..150).random().dp })
+                    .background(Color(Random.nextInt()))
+            )
+        }
+    }
+}
