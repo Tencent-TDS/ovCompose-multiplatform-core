c5a02bd2e6f1fdca97d0f136a80d0b0bd686cab6
dima.avdeev <dima.avdeev@jetbrains.com>
2023-05-09 19:50:15 +0400

    val WindowInsets.Companion.iosSafeArea: WindowInsets

--- compose/foundation/foundation-layout/src/uikitMain/kotlin/androidx/compose/foundation/layout/WindowInsets.uikit.kt
+++ compose/foundation/foundation-layout/src/uikitMain/kotlin/androidx/compose/foundation/layout/WindowInsets.uikit.kt
@@ -34,23 +34,22 @@ import androidx.compose.ui.unit.LayoutDirection
  *
  * This property should be set prior to first composition.
  */
-var ComposeView.consumeWindowInsets: Boolean
-    get() = getTag(R.id.consume_window_insets_tag) as? Boolean ?: true
-    set(value) {
-        setTag(R.id.consume_window_insets_tag, value)
-    }
+//var ComposeView.consumeWindowInsets: Boolean
+//    get() = getTag(R.id.consume_window_insets_tag) as? Boolean ?: true
+//    set(value) {
+//        setTag(R.id.consume_window_insets_tag, value)
+//    }
 
 /**
- * For the [WindowInsetsCompat.Type.displayCutout]. This insets represents the area that the
- * display cutout (e.g. for camera) is and important content should be excluded from.
+ * This insets represents the iOS SafeArea
  */
-val WindowInsets.Companion.displayCutout: WindowInsets
+val WindowInsets.Companion.iosSafeArea: WindowInsets
     @Composable
     @NonRestartableComposable
-    get() = WindowInsetsHolder.current().displayCutout
+    get() = TODO()
 
 /**
- * For the [WindowInsetsCompat.Type.ime]. On API level 23 (M) and above, the soft keyboard can be
+ * On API level 23 (M) and above, the soft keyboard can be
  * detected and [ime] will update when it shows. On API 30 (R) and above, the [ime] insets will
  * animate synchronously with the actual IME animation.
  *
@@ -61,250 +60,24 @@ val WindowInsets.Companion.displayCutout: WindowInsets
 val WindowInsets.Companion.ime: WindowInsets
     @Composable
     @NonRestartableComposable
-    get() = WindowInsetsHolder.current().ime
+    get() = TODO()
 
-/**
- * For the [WindowInsetsCompat.Type.mandatorySystemGestures]. These insets represents the
- * space where system gestures have priority over application gestures.
- */
-val WindowInsets.Companion.mandatorySystemGestures: WindowInsets
-    @Composable
-    @NonRestartableComposable
-    get() = WindowInsetsHolder.current().mandatorySystemGestures
-
-/**
- * For the [WindowInsetsCompat.Type.navigationBars]. These insets represent where
- * system UI places navigation bars. Interactive UI should avoid the navigation bars
- * area.
- */
-val WindowInsets.Companion.navigationBars: WindowInsets
-    @Composable
-    @NonRestartableComposable
-    get() = WindowInsetsHolder.current().navigationBars
-
-/**
- * For the [WindowInsetsCompat.Type.statusBars].
- */
-val WindowInsets.Companion.statusBars: WindowInsets
-    @Composable
-    @NonRestartableComposable
-    get() = WindowInsetsHolder.current().statusBars
-
-/**
- * For the [WindowInsetsCompat.Type.systemBars].
- */
-val WindowInsets.Companion.systemBars: WindowInsets
-    @Composable
-    @NonRestartableComposable
-    get() = WindowInsetsHolder.current().systemBars
-
-/**
- * For the [WindowInsetsCompat.Type.systemGestures].
- */
-val WindowInsets.Companion.systemGestures: WindowInsets
-    @Composable
-    @NonRestartableComposable
-    get() = WindowInsetsHolder.current().systemGestures
-
-/**
- * For the [WindowInsetsCompat.Type.tappableElement].
- */
-val WindowInsets.Companion.tappableElement: WindowInsets
-    @Composable
-    @NonRestartableComposable
-    get() = WindowInsetsHolder.current().tappableElement
-
-/**
- * The insets that include areas where content may be covered by other drawn content.
- * This includes all [system bars][systemBars], [display cutout][displayCutout], and
- * [soft keyboard][ime].
- */
-val WindowInsets.Companion.safeDrawing: WindowInsets
-    @Composable
-    @NonRestartableComposable
-    get() = WindowInsetsHolder.current().safeDrawing
-
-/**
- * The insets that include areas where gestures may be confused with other input,
- * including [system gestures][systemGestures],
- * [mandatory system gestures][mandatorySystemGestures],
- * [rounded display areas][waterfall], and [tappable areas][tappableElement].
- */
-val WindowInsets.Companion.safeGestures: WindowInsets
-    @Composable
-    @NonRestartableComposable
-    get() = WindowInsetsHolder.current().safeGestures
-
-/**
- * The insets that include all areas that may be drawn over or have gesture confusion,
- * including everything in [safeDrawing] and [safeGestures].
- */
-val WindowInsets.Companion.safeContent: WindowInsets
-    @Composable
-    @NonRestartableComposable
-    get() = WindowInsetsHolder.current().safeContent
-
-/**
- * The insets that the [WindowInsetsCompat.Type.captionBar] will consume if shown.
- * If it cannot be shown then this will be empty.
- */
-@ExperimentalLayoutApi
-val WindowInsets.Companion.captionBarIgnoringVisibility: WindowInsets
-    @Suppress("OPT_IN_MARKER_ON_WRONG_TARGET")
-    @ExperimentalLayoutApi
-    @Composable
-    @NonRestartableComposable
-    get() = WindowInsetsHolder.current().captionBarIgnoringVisibility
-
-/**
- * The insets that [WindowInsetsCompat.Type.navigationBars] will consume if shown.
- * These insets represent where system UI places navigation bars. Interactive UI should
- * avoid the navigation bars area. If navigation bars cannot be shown, then this will be
- * empty.
- */
-@ExperimentalLayoutApi
-val WindowInsets.Companion.navigationBarsIgnoringVisibility: WindowInsets
-    @Suppress("OPT_IN_MARKER_ON_WRONG_TARGET")
-    @ExperimentalLayoutApi
-    @Composable
-    @NonRestartableComposable
-    get() = WindowInsetsHolder.current().navigationBarsIgnoringVisibility
-
-/**
- * The insets that [WindowInsetsCompat.Type.statusBars] will consume if shown.
- * If the status bar can never be shown, then this will be empty.
- */
-@ExperimentalLayoutApi
-val WindowInsets.Companion.statusBarsIgnoringVisibility: WindowInsets
-    @Suppress("OPT_IN_MARKER_ON_WRONG_TARGET")
-    @ExperimentalLayoutApi
-    @Composable
-    @NonRestartableComposable
-    get() = WindowInsetsHolder.current().statusBarsIgnoringVisibility
-
-/**
- * The insets that [WindowInsetsCompat.Type.systemBars] will consume if shown.
- *
- * If system bars can never be shown, then this will be empty.
- */
-@ExperimentalLayoutApi
-val WindowInsets.Companion.systemBarsIgnoringVisibility: WindowInsets
-    @Suppress("OPT_IN_MARKER_ON_WRONG_TARGET")
-    @ExperimentalLayoutApi
-    @Composable
-    @NonRestartableComposable
-    get() = WindowInsetsHolder.current().systemBarsIgnoringVisibility
-
-/**
- * The insets that [WindowInsetsCompat.Type.tappableElement] will consume if active.
- *
- * If there are never tappable elements then this is empty.
- */
-@ExperimentalLayoutApi
-val WindowInsets.Companion.tappableElementIgnoringVisibility: WindowInsets
-    @Suppress("OPT_IN_MARKER_ON_WRONG_TARGET")
-    @ExperimentalLayoutApi
-    @Composable
-    @NonRestartableComposable
-    get() = WindowInsetsHolder.current().tappableElementIgnoringVisibility
-
-/**
- * `true` when the [caption bar][captionBar] is being displayed, irrespective of
- * whether it intersects with the Window.
- */
-@ExperimentalLayoutApi
-val WindowInsets.Companion.isCaptionBarVisible: Boolean
-    @Suppress("OPT_IN_MARKER_ON_WRONG_TARGET")
-    @ExperimentalLayoutApi
-    @Composable
-    @NonRestartableComposable
-    get() = WindowInsetsHolder.current().captionBar.isVisible
-
-/**
- * `true` when the [soft keyboard][ime] is being displayed, irrespective of
- * whether it intersects with the Window.
- */
-@ExperimentalLayoutApi
-val WindowInsets.Companion.isImeVisible: Boolean
-    @Suppress("OPT_IN_MARKER_ON_WRONG_TARGET")
-    @ExperimentalLayoutApi
-    @Composable
-    @NonRestartableComposable
-    get() = WindowInsetsHolder.current().ime.isVisible
-
-/**
- * `true` when the [statusBars] are being displayed, irrespective of
- * whether they intersects with the Window.
- */
-@ExperimentalLayoutApi
-val WindowInsets.Companion.areStatusBarsVisible: Boolean
-    @Suppress("OPT_IN_MARKER_ON_WRONG_TARGET")
-    @ExperimentalLayoutApi
-    @Composable
-    @NonRestartableComposable
-    get() = WindowInsetsHolder.current().statusBars.isVisible
-
-/**
- * `true` when the [navigationBars] are being displayed, irrespective of
- * whether they intersects with the Window.
- */
-@ExperimentalLayoutApi
-val WindowInsets.Companion.areNavigationBarsVisible: Boolean
-    @Suppress("OPT_IN_MARKER_ON_WRONG_TARGET")
-    @ExperimentalLayoutApi
-    @Composable
-    @NonRestartableComposable
-    get() = WindowInsetsHolder.current().navigationBars.isVisible
-
-/**
- * `true` when the [systemBars] are being displayed, irrespective of
- * whether they intersects with the Window.
- */
-@ExperimentalLayoutApi
-val WindowInsets.Companion.areSystemBarsVisible: Boolean
-    @Suppress("OPT_IN_MARKER_ON_WRONG_TARGET")
-    @ExperimentalLayoutApi
-    @Composable
-    @NonRestartableComposable
-    get() = WindowInsetsHolder.current().systemBars.isVisible
-/**
- * `true` when the [tappableElement] is being displayed, irrespective of
- * whether they intersects with the Window.
- */
-@ExperimentalLayoutApi
-val WindowInsets.Companion.isTappableElementVisible: Boolean
-    @Suppress("OPT_IN_MARKER_ON_WRONG_TARGET")
-    @ExperimentalLayoutApi
-    @Composable
-    @NonRestartableComposable
-    get() = WindowInsetsHolder.current().tappableElement.isVisible
-
-/**
- * The [WindowInsets] for the IME before the IME started animating in. The current
- * animated value is [WindowInsets.Companion.ime].
- *
- * This will be the same as [imeAnimationTarget] when there is no IME animation
- * in progress.
- */
-@ExperimentalLayoutApi
-val WindowInsets.Companion.imeAnimationSource: WindowInsets
-    @Suppress("OPT_IN_MARKER_ON_WRONG_TARGET")
-    @ExperimentalLayoutApi
-    @Composable
-    @NonRestartableComposable
-    get() = WindowInsetsHolder.current().imeAnimationSource
-
-/**
- * The [WindowInsets] for the IME when the animation completes, if it is allowed
- * to complete successfully. The current animated value is [WindowInsets.Companion.ime].
- *
- * This will be the same as [imeAnimationSource] when there is no IME animation
- * in progress.
- */
-@ExperimentalLayoutApi
-val WindowInsets.Companion.imeAnimationTarget: WindowInsets
-    @Suppress("OPT_IN_MARKER_ON_WRONG_TARGET")
-    @ExperimentalLayoutApi
-    @Composable
-    @NonRestartableComposable
-    get() = WindowInsetsHolder.current().imeAnimationTarget
+///**
+// * The insets that include areas where gestures may be confused with other input,
+// * including [system gestures][systemGestures],
+// * [mandatory system gestures][mandatorySystemGestures],
+// * [rounded display areas][waterfall], and [tappable areas][tappableElement].
+// */
+//val WindowInsets.Companion.safeGestures: WindowInsets
+//    @Composable
+//    @NonRestartableComposable
+//    get() = WindowInsetsHolder.current().safeGestures
+//
+///**
+// * The insets that include all areas that may be drawn over or have gesture confusion,
+// * including everything in [safeDrawing] and [safeGestures].
+// */
+//val WindowInsets.Companion.safeContent: WindowInsets
+//    @Composable
+//    @NonRestartableComposable
+//    get() = WindowInsetsHolder.current().safeContent
