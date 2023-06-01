57da335f7958d49cd7b89d2113e7389d1d3f142a
Ivan Matkov <ivan.matkov@jetbrains.com>
2023-06-01 10:42:31 +0200

    Move app layout mpp example to common

--- compose/mpp/demo/src/uikitMain/kotlin/androidx/compose/mpp/demo/main.uikit.kt
+++ compose/mpp/demo/src/uikitMain/kotlin/androidx/compose/mpp/demo/main.uikit.kt
@@ -1,26 +1,13 @@
 // Use `xcodegen` first, then `open ./SkikoSample.xcodeproj` and then Run button in XCode.
 package androidx.compose.mpp.demo
 
-import ApplicationLayoutExamples
-import androidx.compose.runtime.*
+import androidx.compose.runtime.remember
 import androidx.compose.ui.main.defaultUIKitMain
 import androidx.compose.ui.window.ComposeUIViewController
 
 fun main() {
     defaultUIKitMain("ComposeDemo", ComposeUIViewController {
-        IosDemo()
+        val app = remember { App() }
+        app.Content()
     })
-}
-
-@Composable
-fun IosDemo() {
-    // You may uncomment different examples:
-//    MultiplatformDemo()
-    ApplicationLayoutExamples()
-}
-
-@Composable
-fun MultiplatformDemo() {
-    val app = remember { App() }
-    app.Content()
 }
