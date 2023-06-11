0bc990288243d556a5d6c5be62da30b75b131393
dima.avdeev <dima.avdeev@jetbrains.com>
2023-06-11 17:27:53 +0400

    fix ios demo compilation

--- compose/mpp/demo/src/uikitMain/kotlin/androidx/compose/mpp/demo/main.uikit.kt
+++ compose/mpp/demo/src/uikitMain/kotlin/androidx/compose/mpp/demo/main.uikit.kt
@@ -2,18 +2,37 @@
 package androidx.compose.mpp.demo
 
 import NativeModalWithNaviationExample
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.main.defaultUIKitMain
 import androidx.compose.ui.window.ComposeUIViewController
 
 
 fun main() {
     defaultUIKitMain("ComposeDemo", ComposeUIViewController {
-        val app = remember {
-            App(extraScreens = listOf(
-                NativeModalWithNaviationExample
-            ))
-        }
-        app.Content()
+        IosDemo()
     })
+}
+
+@Composable
+fun IosDemo() {
+    var isApplicationLayout by remember { mutableStateOf(true) }
+    if (isApplicationLayout) {
+        ApplicationLayouts {
+            isApplicationLayout = false
+        }
+    } else {
+        MultiplatformDemo()
+    }
+}
+
+@Composable
+private fun MultiplatformDemo() {
+    val app = remember {
+        App(
+            extraScreens = listOf(
+                NativeModalWithNaviationExample
+            )
+        )
+    }
+    app.Content()
 }
