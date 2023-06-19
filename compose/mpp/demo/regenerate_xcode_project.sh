4900222b3b079d60c4822aa35dd05c0ae5ee1599
Elijah Semyonov <elijah.semyonov@jetbrains.com>
2023-06-19 14:12:50 +0200

    Explicitly pass environmental variables to Xcode build script.

--- compose/mpp/demo/regenerate_xcode_project.sh
+++ compose/mpp/demo/regenerate_xcode_project.sh
@@ -5,16 +5,28 @@ cd "$(dirname "$0")" || exit
 
 if command -v xcodegen >/dev/null 2>&1; then
   # xcodegen exists
+  PROJECT_NAME=ComposeDemo
 
-  projPath="ComposeDemo.xcodeproj"
-
-  if [ -d "$projPath" ]; then
-    echo "Removing existing project"
-    rm -rf "$projPath"
+  if [[ -z "$JAVA_HOME" ]]; then
+    echo "JAVA_HOME is not set"
+    exit 1
   fi
 
-  xcodegen
-  open $projPath
+  PROJECT_FILE_PATH="$PROJECT_NAME.xcodeproj"
+
+  if [ -d "$PROJECT_FILE_PATH" ]; then
+    echo "Removing existing project"
+    rm -rf "$PROJECT_FILE_PATH"
+  fi
+
+  INPUT_FILE="project.template.yml"
+  OUTPUT_FILE="project.generated.yml"
+
+  # replace template placeholders with actual values
+  sed -e "s|%@JAVA_HOME@%|$JAVA_HOME|g" -e "s|%@PROJECT_NAME@%|$PROJECT_NAME|g" $INPUT_FILE > $OUTPUT_FILE
+
+  xcodegen --spec $OUTPUT_FILE
+  open $PROJECT_FILE_PATH
 else
   # xcodegen does not exist
   echo "Error: xcodegen not found. Please install it using 'brew install xcodegen'."
