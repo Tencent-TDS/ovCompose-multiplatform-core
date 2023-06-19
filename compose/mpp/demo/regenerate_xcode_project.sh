29a9200cda23bb1272af35d8d82845ce6685690c
Elijah Semyonov <elijah.semyonov@jetbrains.com>
2023-06-19 14:21:58 +0200

    Integrate COMPOSE_DEMO_APPLE_TEAM_ID variable into xcodegen

--- compose/mpp/demo/regenerate_xcode_project.sh
+++ compose/mpp/demo/regenerate_xcode_project.sh
@@ -7,6 +7,11 @@ if command -v xcodegen >/dev/null 2>&1; then
   # xcodegen exists
   PROJECT_NAME=ComposeDemo
 
+  if [[ -z "$COMPOSE_DEMO_APPLE_TEAM_ID" ]]; then
+    echo "COMPOSE_DEMO_APPLE_TEAM_ID is not set"
+    COMPOSE_DEMO_APPLE_TEAM_ID="N462MKSJ7M"
+  fi
+
   if [[ -z "$JAVA_HOME" ]]; then
     echo "JAVA_HOME is not set"
     exit 1
@@ -23,7 +28,7 @@ if command -v xcodegen >/dev/null 2>&1; then
   OUTPUT_FILE="project.generated.yml"
 
   # replace template placeholders with actual values
-  sed -e "s|%@JAVA_HOME@%|$JAVA_HOME|g" -e "s|%@PROJECT_NAME@%|$PROJECT_NAME|g" $INPUT_FILE > $OUTPUT_FILE
+  sed -e "s|%@JAVA_HOME@%|$JAVA_HOME|g" -e "s|%@PROJECT_NAME@%|$PROJECT_NAME|g" -e "s|%@COMPOSE_DEMO_APPLE_TEAM_ID@%|$COMPOSE_DEMO_APPLE_TEAM_ID|g" $INPUT_FILE > $OUTPUT_FILE
 
   xcodegen --spec $OUTPUT_FILE
   open $PROJECT_FILE_PATH
