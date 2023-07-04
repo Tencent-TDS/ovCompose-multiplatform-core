838dcd5b3483ec11cbb988c29f1996d70fa8a56c
Elijah Semyonov <elijah.semyonov@jetbrains.com>
2023-07-04 14:39:14 +0200

    Revert visibility modifier change.

--- compose/foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/gestures/Scrollable.kt
+++ compose/foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/gestures/Scrollable.kt
@@ -615,7 +615,7 @@ private class ScrollingLogic(
         return result
     }
 
-    private suspend fun doFlingAnimationInNewScrollScope(available: Velocity): Velocity {
+    suspend fun doFlingAnimationInNewScrollScope(available: Velocity): Velocity {
         var result: Velocity = available
 
         scrollableState.scroll {
