a59d658da2959788fb52ba62c6523bca6fec1e3e
Ivan Matkov <ivan.matkov@jetbrains.com>
2023-07-18 11:05:57 +0200

    Fix release handling

--- compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/ComposeScene.skiko.kt
+++ compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/ComposeScene.skiko.kt
@@ -582,11 +582,8 @@ class ComposeScene internal constructor(
     }
 
     private fun processRelease(event: PointerInputEvent) {
-        val owner = pressOwner ?: hoveredOwner(event)
-        if (focusedOwner.isAbove(owner)) {
-            return
-        }
-        owner?.processPointerInput(event)
+        // Send Release to pressOwner even if is not hovered or under focused.
+        pressOwner?.processPointerInput(event)
     }
 
     private fun processMove(event: PointerInputEvent) {
@@ -596,6 +593,7 @@ class ComposeScene internal constructor(
             else -> hoveredOwner(event)
         }
         if (focusedOwner.isAbove(owner)) {
+            // If pressOwner is under focusedOwner, hover state must be updated
             owner = null
         }
 
