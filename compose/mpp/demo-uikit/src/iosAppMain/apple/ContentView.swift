079b4763a58fa2f94def12f0b9251ccddeaf9d19
dima.avdeev <dima.avdeev@jetbrains.com>
2023-08-17 12:38:10 +0400

    Reproduce keyboard empty whitespace bug
    Issue https://github.com/JetBrains/compose-multiplatform/issues/3490

--- compose/mpp/demo-uikit/src/iosAppMain/apple/ContentView.swift
+++ compose/mpp/demo-uikit/src/iosAppMain/apple/ContentView.swift
@@ -20,7 +20,7 @@ import shared
 
 struct ContentView: View {
     var body: some View {
-        ComposeView().ignoresSafeArea(.keyboard)
+        ComposeView().ignoresSafeArea(.all, edges: .top).ignoresSafeArea(.keyboard)
     }
 }
 
