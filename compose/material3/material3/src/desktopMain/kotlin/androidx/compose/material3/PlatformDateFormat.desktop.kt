cd10a8e3330743661aa69d8327c2ff41e7b089ca
Aleksandr Zhirkevich <sasha.zhirkevich@gmail.com>
2023-08-22 01:42:22 +0300

    stub for dekstop ugly format

--- compose/material3/material3/src/desktopMain/kotlin/androidx/compose/material3/PlatformDateFormat.desktop.kt
+++ compose/material3/material3/src/desktopMain/kotlin/androidx/compose/material3/PlatformDateFormat.desktop.kt
@@ -47,8 +47,8 @@ internal actual object PlatformDateFormat {
         // stub: not localized but at least readable variant
         val pattern = when(skeleton){
             DatePickerDefaults.YearMonthSkeleton -> "MMMM yyyy"
-            DatePickerDefaults.YearAbbrMonthDaySkeleton -> "dd MMM yyyy"
-            DatePickerDefaults.YearMonthWeekdayDaySkeleton -> "EEEE dd MMMM yyyy"
+            DatePickerDefaults.YearAbbrMonthDaySkeleton -> "d MMM yyyy"
+            DatePickerDefaults.YearMonthWeekdayDaySkeleton -> "EEEE, d MMMM yyyy"
             else -> skeleton
         }
         return formatWithPattern(utcTimeMillis, pattern, locale)
