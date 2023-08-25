8cc2cf96688675a5f968f1d626325eaa0e82e1d2
Elijah Semyonov <elijah.semyonov@jetbrains.com>
2023-08-25 11:41:14 +0200

    Implement iOS native look and feel for text fields

--- compose/foundation/foundation/src/uikitMain/kotlin/androidx/compose/foundation/cupertino/CupertinoOverscrollEffect.kt
+++ compose/foundation/foundation/src/uikitMain/kotlin/androidx/compose/foundation/cupertino/CupertinoOverscrollEffect.kt
@@ -349,6 +349,7 @@ class CupertinoOverscrollEffect(
             animationSpec = spec
         ) {
             overscrollOffset = (value * density).toOffset()
+            println(overscrollOffset)
             currentVelocity = velocity * density
 
             // If it was fling from overscroll, cancel animation and return velocity
