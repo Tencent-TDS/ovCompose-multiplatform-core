31b47d1c7332a409c29ec5dcd46d6b31decf98d5
Elijah Semyonov <elijah.semyonov@jetbrains.com>
2023-09-07 11:32:36 +0200

    Move check to low priority dispatch queue

--- compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/uikit/PlistSanityCheck.kt
+++ compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/uikit/PlistSanityCheck.kt
@@ -19,18 +19,26 @@ package androidx.compose.ui.uikit
 import kotlinx.atomicfu.atomic
 import platform.Foundation.NSBundle
 import platform.Foundation.NSNumber
+import platform.darwin.DISPATCH_QUEUE_PRIORITY_LOW
+import platform.darwin.dispatch_async
+import platform.darwin.dispatch_get_global_queue
 
 internal object PlistSanityCheck {
     private var isPerformed = atomic(false)
 
     fun performIfNeeded() {
         if (isPerformed.compareAndSet(expect = false, update = true)) {
-            val entry = NSBundle
-                .mainBundle
-                .objectForInfoDictionaryKey("CADisableMinimumFrameDurationOnPhone") as? NSNumber
+            dispatch_async(dispatch_get_global_queue(
+                DISPATCH_QUEUE_PRIORITY_LOW.toLong(),
+                0u
+            )) {
+                val entry = NSBundle
+                    .mainBundle
+                    .objectForInfoDictionaryKey("CADisableMinimumFrameDurationOnPhone") as? NSNumber
 
-            if (entry?.boolValue != true) {
-                println("WARNING: `Info.plist` doesn't have a valid `CADisableMinimumFrameDurationOnPhone` entry. Framerate will be restricted to 60hz on iPhones. To support high frequency rendering on iPhones, add `CADisableMinimumFrameDurationOnPhone` entry to `Info.plist` and set it to YES.")
+                if (entry?.boolValue != true) {
+                    println("WARNING: `Info.plist` doesn't have a valid `CADisableMinimumFrameDurationOnPhone` entry. Framerate will be restricted to 60hz on iPhones. To support high frequency rendering on iPhones, add `CADisableMinimumFrameDurationOnPhone` entry to `Info.plist` and set it to YES.")
+                }
             }
         }
     }
