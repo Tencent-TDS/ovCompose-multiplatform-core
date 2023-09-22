45ef267224d73973738f2428c59caa1cd4760a03
Ivan Matkov <ivan.matkov@jetbrains.com>
2023-09-22 11:45:29 +0200

    Fix composable context

--- compose/foundation/foundation-layout/src/skikoMain/kotlin/androidx/compose/foundation/layout/WindowInsetsPadding.skiko.kt
+++ compose/foundation/foundation-layout/src/skikoMain/kotlin/androidx/compose/foundation/layout/WindowInsetsPadding.skiko.kt
@@ -16,6 +16,7 @@
 
 package androidx.compose.foundation.layout
 
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -24,49 +25,71 @@ import androidx.compose.ui.platform.InspectorInfo
 import androidx.compose.ui.platform.debugInspectorInfo
 
 actual fun Modifier.safeDrawingPadding() =
-    windowInsetsPadding(debugInspectorInfo { name = "safeDrawingPadding" }) { safeDrawing }
+    windowInsetsPadding(debugInspectorInfo { name = "safeDrawingPadding" }) {
+        WindowInsets.safeDrawing
+    }
 
 actual fun Modifier.safeGesturesPadding() =
-    windowInsetsPadding(debugInspectorInfo { name = "safeGesturesPadding" }) { safeGestures }
+    windowInsetsPadding(debugInspectorInfo { name = "safeGesturesPadding" }) {
+        WindowInsets.safeGestures
+    }
 
 actual fun Modifier.safeContentPadding() =
-    windowInsetsPadding(debugInspectorInfo { name = "safeContentPadding" }) { safeContent }
+    windowInsetsPadding(debugInspectorInfo { name = "safeContentPadding" }) {
+        WindowInsets.safeContent
+    }
 
 actual fun Modifier.systemBarsPadding() =
-    windowInsetsPadding(debugInspectorInfo { name = "systemBarsPadding" }) { systemBars }
+    windowInsetsPadding(debugInspectorInfo { name = "systemBarsPadding" }) {
+        WindowInsets.systemBars
+    }
 
 actual fun Modifier.displayCutoutPadding() =
-    windowInsetsPadding(debugInspectorInfo { name = "displayCutoutPadding" }) { displayCutout }
+    windowInsetsPadding(debugInspectorInfo { name = "displayCutoutPadding" }) {
+        WindowInsets.displayCutout
+    }
 
 actual fun Modifier.statusBarsPadding() =
-    windowInsetsPadding(debugInspectorInfo { name = "statusBarsPadding" }) { statusBars }
+    windowInsetsPadding(debugInspectorInfo { name = "statusBarsPadding" }) {
+        WindowInsets.statusBars
+    }
 
 actual fun Modifier.imePadding() =
-    windowInsetsPadding(debugInspectorInfo { name = "imePadding" }) { ime }
+    windowInsetsPadding(debugInspectorInfo { name = "imePadding" }) {
+        WindowInsets.ime
+    }
 
 actual fun Modifier.navigationBarsPadding() =
-    windowInsetsPadding(debugInspectorInfo { name = "navigationBarsPadding" }) { navigationBars }
+    windowInsetsPadding(debugInspectorInfo { name = "navigationBarsPadding" }) {
+        WindowInsets.navigationBars
+    }
 
 actual fun Modifier.captionBarPadding() =
-    windowInsetsPadding(debugInspectorInfo { name = "captionBarPadding" }) { captionBar }
+    windowInsetsPadding(debugInspectorInfo { name = "captionBarPadding" }) {
+        WindowInsets.captionBar
+    }
 
 actual fun Modifier.waterfallPadding() =
-    windowInsetsPadding(debugInspectorInfo { name = "waterfallPadding" }) { waterfall }
+    windowInsetsPadding(debugInspectorInfo { name = "waterfallPadding" }) {
+        WindowInsets.waterfall
+    }
 
 actual fun Modifier.systemGesturesPadding() =
-    windowInsetsPadding(debugInspectorInfo { name = "systemGesturesPadding" }) { systemGestures }
+    windowInsetsPadding(debugInspectorInfo { name = "systemGesturesPadding" }) {
+        WindowInsets.systemGestures
+    }
 
 actual fun Modifier.mandatorySystemGesturesPadding() =
     windowInsetsPadding(debugInspectorInfo { name = "mandatorySystemGesturesPadding" }) {
-        mandatorySystemGestures
+        WindowInsets.mandatorySystemGestures
     }
 
 @Suppress("NOTHING_TO_INLINE")
 @Stable
 private inline fun Modifier.windowInsetsPadding(
     noinline inspectorInfo: InspectorInfo.() -> Unit,
-    crossinline insetsCalculation: WindowInsets.Companion.() -> WindowInsets
+    crossinline insetsCalculation: @Composable () -> WindowInsets
 ): Modifier = composed(inspectorInfo) {
-    val insets = WindowInsets.Companion.insetsCalculation()
+    val insets = insetsCalculation()
     InsetsPaddingModifier(insets)
 }
