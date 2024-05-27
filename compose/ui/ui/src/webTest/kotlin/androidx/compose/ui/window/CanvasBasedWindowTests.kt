9a75c20c7b8ecd72aecd0079df3cefa7f28c5e4b
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2024-05-27 11:26:26 +0200

    [web] Introduce number numpad section to KeyEventConversionTests

--- compose/ui/ui/src/webTest/kotlin/androidx/compose/ui/window/CanvasBasedWindowTests.kt
+++ compose/ui/ui/src/webTest/kotlin/androidx/compose/ui/window/CanvasBasedWindowTests.kt
@@ -44,7 +44,6 @@ import kotlin.test.AfterTest
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 import kotlin.test.assertFalse
-import kotlinx.browser.window
 
 class CanvasBasedWindowTests {
 
