6c2154d4263423b4ae18fa5fa6ba0415cf110d66
Elijah Semyonov <elijah.semyonov@jetbrains.com>
2024-08-14 10:06:49 +0200

    Add documentation

--- compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/viewinterop/UIKitInteropProperties.uikit.kt
+++ compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/viewinterop/UIKitInteropProperties.uikit.kt
@@ -62,6 +62,14 @@ data class UIKitInteropProperties @ExperimentalComposeUiApi constructor(
     internal val isInteractive: Boolean
         get() = interactionMode != null
 
+    /**
+     * @param isInteractive Indicates whether the affected interop component should process touches
+     * or not. If true, the default strategy with delay will be used. Otherwise, user interaction is
+     * disabled for it.
+     *
+     * @param isNativeAccessibilityEnabled Indicates whether native acessibility resolution is
+     * enabled for the interop component. For more details see [UIKitInteropProperties].
+     */
     constructor(
         isInteractive: Boolean,
         isNativeAccessibilityEnabled: Boolean
