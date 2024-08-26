1926a7c799c076319d200d557b91c550ea96ca2d
Elijah Semyonov <elijah.semyonov@jetbrains.com>
2024-08-26 13:57:30 +0200

    Modify example

--- compose/mpp/demo/src/uikitMain/kotlin/androidx/compose/mpp/demo/UpdatableInteropPropertiesExample.kt
+++ compose/mpp/demo/src/uikitMain/kotlin/androidx/compose/mpp/demo/UpdatableInteropPropertiesExample.kt
@@ -17,8 +17,6 @@
 package androidx.compose.mpp.demo
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
