21e6636dfc0a99d1bcc5944397e5433571164a70
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2024-10-09 15:06:38 +0200

    Fix formatting

--- navigation/navigation-runtime/src/webMain/kotlin/androidx/navigation/BrowserHistory.kt
+++ navigation/navigation-runtime/src/webMain/kotlin/androidx/navigation/BrowserHistory.kt
@@ -27,8 +27,8 @@ import kotlinx.coroutines.launch
 /**
  * Binds the browser window state to the given navigation controller.
  *
- * @param navController The `NavController` instance to bind to browser window navigation.
- * @param getBackStackEntryPath A function that returns the path to show for a given `NavBackStackEntry`.
+ * @param navController The [NavController] instance to bind to browser window navigation.
+ * @param getBackStackEntryPath A function that returns the path to show for a given [NavBackStackEntry].
  */
 internal suspend fun BrowserWindow.bindToNavigation(
     navController: NavController,
