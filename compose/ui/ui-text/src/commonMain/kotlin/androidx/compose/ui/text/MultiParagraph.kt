xiaocainiaooo/room3

Fix IndexOutOfBound crash with link annotation
When we create nodes for link annotations, we should only be looking within the last visible line and not the full text.

In findParagraphByIndex we need to make sure that the index is within the visible lines. For that we check that it's not greater than last line's end.
Bug: 372390054
Fixes: 374115892
Test: existing tests + demo

Change-Id: I3b1f69541b19c630530846a604c832eb7384847e

compose/ui/ui-text/src/commonMain/kotlin/androidx/compose/ui/text/MultiParagraph.kt
@@ -1014,6 +1014,10 @@ class MultiParagraph(
  * @return The index of the target [ParagraphInfo] in [paragraphInfoList].
  */
 internal fun findParagraphByIndex(paragraphInfoList: List<ParagraphInfo>, index: Int): Int {
+    val lastLineEnd = paragraphInfoList.last().endIndex
+    requirePrecondition(index <= paragraphInfoList.last().endIndex) {
+        "Index $index should be less or equal than last line's end $lastLineEnd"
+    }
     val paragraphIndex =
         paragraphInfoList.fastBinarySearch { paragraphInfo ->
             when {

Tencent-TDS/ovCompose-multiplatform-core

In this change compared to the previous attempt to fix this issue (aosp/3320137) I remove an early return to keep the link's Box composable in composition since early return causes all sorts of issues, e.g. the retaining of the focus.
Test: new tests + demo
(cherry picked from https://android-review.googlesource.com/q/commit:be7605af63fcd036dd037a67a1b18f2166598f6e)
(cherry picked from https://android-review.googlesource.com/q/commit:05953496585570807c2d9c447aeb5d311fcdd4a7)
Merged-In: Ic96d2868e3edd1ca11d718670fd494a8d402e086
Change-Id: Ic96d2868e3edd1ca11d718670fd494a8d402e086

compose/ui/ui-text/src/commonMain/kotlin/androidx/compose/ui/text/MultiParagraph.kt
@@ -933,6 +933,10 @@ class MultiParagraph(
  * @return The index of the target [ParagraphInfo] in [paragraphInfoList].
  */
 internal fun findParagraphByIndex(paragraphInfoList: List<ParagraphInfo>, index: Int): Int {
+    val lastLineEnd = paragraphInfoList.last().endIndex
+    require(index <= paragraphInfoList.last().endIndex) {
+        "Index $index should be less or equal than last line's end $lastLineEnd"
+    }
     return paragraphInfoList.fastBinarySearch { paragraphInfo ->
         when {
             paragraphInfo.startIndex > index -> 1