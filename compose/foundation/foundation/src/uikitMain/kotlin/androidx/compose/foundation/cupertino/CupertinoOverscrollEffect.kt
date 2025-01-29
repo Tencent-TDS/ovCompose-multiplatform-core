a086d56dc97b95640c7bc005a7ea3a847819895e
Andrei.Salavei <andrei.salavei@jetbrains.com>
2025-01-29 10:38:23 +0100

    MR Comments

--- compose/foundation/foundation/src/uikitMain/kotlin/androidx/compose/foundation/cupertino/CupertinoOverscrollEffect.kt
+++ compose/foundation/foundation/src/uikitMain/kotlin/androidx/compose/foundation/cupertino/CupertinoOverscrollEffect.kt
@@ -28,7 +28,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.geometry.toRect
 import androidx.compose.ui.graphics.drawscope.ContentDrawScope
 import androidx.compose.ui.graphics.drawscope.clipRect
 import androidx.compose.ui.input.nestedscroll.NestedScrollSource
@@ -448,17 +450,25 @@ private class CupertinoOverscrollNode(
 
     override fun ContentDrawScope.draw() {
         if (applyClip) {
-            val offset = offset()
+            val bounds = Rect(-offset().toOffset(), size)
+            val rect = size.toRect().intersect(bounds)
             clipRect(
-                top = maxOf(0f, -offset.y.toFloat()),
-                bottom = size.height - maxOf(0, offset.y),
-                left = maxOf(0f, -offset.x.toFloat()),
-                right = size.width - maxOf(0, offset.x)
+                left = rect.left,
+                top = rect.top,
+                right = rect.right,
+                bottom = rect.bottom,
             ) { this@draw.drawContent() }
+//            clipRect(
+//                top = maxOf(0f, -offset.y.toFloat()),
+//                bottom = size.height - maxOf(0, offset.y),
+//                left = maxOf(0f, -offset.x.toFloat()),
+//                right = size.width - maxOf(0, offset.x)
+//            ) { this@draw.drawContent() }
         } else {
             this@draw.drawContent()
         }
     }
+
 
     override fun MeasureScope.measure(
         measurable: Measurable,
