b2a200ba582659ebd8f0b2b5ebc41e2fa59e510a
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2025-03-18 10:03:05 +0100

    Move DomInputStrategy to a separate file

--- compose/ui/ui/src/webCommonW3C/kotlin/androidx/compose/ui/platform/BackingTextArea.kt
+++ compose/ui/ui/src/webCommonW3C/kotlin/androidx/compose/ui/platform/BackingTextArea.kt
@@ -17,17 +17,13 @@
 package androidx.compose.ui.platform
 
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.text.input.CommitTextCommand
-import androidx.compose.ui.text.input.DeleteSurroundingTextInCodePointsCommand
 import androidx.compose.ui.text.input.EditCommand
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.ImeOptions
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.SetComposingTextCommand
 import androidx.compose.ui.text.input.TextFieldValue
 import kotlinx.browser.document
 import org.w3c.dom.HTMLTextAreaElement
-import org.w3c.dom.events.CompositionEvent
 import org.w3c.dom.events.KeyboardEvent
 
 
@@ -38,109 +34,6 @@ internal interface ComposeCommandCommunicator {
     fun sendKeyboardEvent(keyboardEvent: KeyboardEvent): Boolean
 }
 
-
-internal interface DomInputStrategy {
-    val htmlInput: HTMLTextAreaElement
-    fun updateState(textFieldValue: TextFieldValue)
-}
-
-internal class DefaultDomInputStrategy(
-    override val htmlInput: HTMLTextAreaElement,
-    private val composeSender: ComposeCommandCommunicator,
-) : DomInputStrategy {
-    private var editState: EditState = EditState.Default
-
-    init {
-        initEvents()
-    }
-
-    private fun initEvents() {
-        var lastKeydownWasProcessed = false
-
-        htmlInput.addEventListener("keydown", {evt ->
-            evt as KeyboardEvent
-            console.log(evt.type, evt.timeStamp, evt.isComposing, evt)
-
-            if (evt.repeat) {
-                editState = EditState.AccentDialogueMode
-                return@addEventListener
-            }
-
-            // this won't prevent other input-related events, in particular "compositionupdate"
-            // even if it was triggered via key press
-            evt.preventDefault()
-
-            if (editState is EditState.AccentDialogueMode) {
-                return@addEventListener
-            }
-
-            if (editState is EditState.CompositeDialogueMode) {
-                return@addEventListener
-            }
-
-            editState = EditState.WaitingComposeActivity
-
-            lastKeydownWasProcessed = composeSender.sendKeyboardEvent(evt)
-            println("PROCESSED $lastKeydownWasProcessed")
-            if (!lastKeydownWasProcessed) {
-                editState = EditState.Default
-            }
-        })
-
-        htmlInput.addEventListener("beforeinput", { evt ->
-            evt as InputEvent
-            console.log("[binput] %c%s %c %s", "font-weight: bold", evt.inputType, "font-weight: normal", evt.data)
-
-
-            evt.preventDefault()
-        })
-
-        htmlInput.addEventListener("input", { evt ->
-            evt as InputEvent
-            console.log("[input] %c%s %c %s", "font-weight: bold", evt.inputType, "font-weight: normal", evt.data)
-
-            evt.preventDefault()
-        })
-
-        htmlInput.addEventListener("compositionstart", {evt ->
-            evt as CompositionEvent
-            console.log(evt.type, evt.timeStamp, evt.data)
-            if (lastKeydownWasProcessed) {
-                composeSender.sendEditCommand(DeleteSurroundingTextInCodePointsCommand(1, 0))
-            }
-
-            editState  = EditState.CompositeDialogueMode
-        })
-
-        htmlInput.addEventListener("compositionupdate", {evt ->
-            evt as CompositionEvent
-            console.log(evt.type, evt.timeStamp, evt.data)
-
-            composeSender.sendEditCommand(SetComposingTextCommand(evt.data, 1))
-            evt.preventDefault()
-        })
-
-        htmlInput.addEventListener("compositionend", {evt ->
-            evt as CompositionEvent
-            console.log(evt.type, evt.timeStamp, evt.data)
-            evt.preventDefault()
-
-            editState = EditState.WaitingComposeActivity
-            composeSender.sendEditCommand(CommitTextCommand(evt.data, 1))
-        })
-    }
-
-    override fun updateState(textFieldValue: TextFieldValue) {
-        if (editState != EditState.WaitingComposeActivity) return
-
-        println("updateState $editState ${textFieldValue.text}")
-
-        htmlInput.value = textFieldValue.text
-        htmlInput.setSelectionRange(textFieldValue.selection.start, textFieldValue.selection.end)
-
-        editState = EditState.Default
-    }
-}
 
 /**
  * The purpose of this entity is to isolate synchronization between a TextFieldValue
@@ -181,13 +74,6 @@ internal class BackingTextArea(
     fun dispose() {
         textArea.remove()
     }
-}
-
-private sealed interface EditState {
-    data object Default : EditState
-    data object WaitingComposeActivity : EditState
-    data object CompositeDialogueMode: EditState
-    data object AccentDialogueMode: EditState
 }
 
 internal fun ImeOptions.createDomElement(): HTMLTextAreaElement {
---
+++ compose/ui/ui/src/webCommonW3C/kotlin/androidx/compose/ui/platform/DomInputStrategy.kt
@@ -0,0 +1,120 @@
+package androidx.compose.ui.platform
+
+import androidx.compose.ui.text.input.CommitTextCommand
+import androidx.compose.ui.text.input.DeleteSurroundingTextInCodePointsCommand
+import androidx.compose.ui.text.input.SetComposingTextCommand
+import androidx.compose.ui.text.input.TextFieldValue
+import org.w3c.dom.HTMLTextAreaElement
+import org.w3c.dom.events.CompositionEvent
+import org.w3c.dom.events.KeyboardEvent
+
+internal interface DomInputStrategy {
+    val htmlInput: HTMLTextAreaElement
+    fun updateState(textFieldValue: TextFieldValue)
+}
+
+internal class DefaultDomInputStrategy(
+    override val htmlInput: HTMLTextAreaElement,
+    private val composeSender: ComposeCommandCommunicator,
+) : DomInputStrategy {
+    private var editState: EditState = EditState.Default
+
+    init {
+        initEvents()
+    }
+
+    private fun initEvents() {
+        var lastKeydownWasProcessed = false
+
+        htmlInput.addEventListener("keydown", {evt ->
+            evt as KeyboardEvent
+            console.log(evt.type, evt.timeStamp, evt.isComposing, evt)
+
+            if (evt.repeat) {
+                editState = EditState.AccentDialogueMode
+                return@addEventListener
+            }
+
+            // this won't prevent other input-related events, in particular "compositionupdate"
+            // even if it was triggered via key press
+            evt.preventDefault()
+
+            if (editState is EditState.AccentDialogueMode) {
+                return@addEventListener
+            }
+
+            if (editState is EditState.CompositeDialogueMode) {
+                return@addEventListener
+            }
+
+            editState = EditState.WaitingComposeActivity
+
+            lastKeydownWasProcessed = composeSender.sendKeyboardEvent(evt)
+            println("PROCESSED $lastKeydownWasProcessed")
+            if (!lastKeydownWasProcessed) {
+                editState = EditState.Default
+            }
+        })
+
+        htmlInput.addEventListener("beforeinput", { evt ->
+            evt as InputEvent
+            console.log("[binput] %c%s %c %s", "font-weight: bold", evt.inputType, "font-weight: normal", evt.data)
+
+
+            evt.preventDefault()
+        })
+
+        htmlInput.addEventListener("input", { evt ->
+            evt as InputEvent
+            console.log("[input] %c%s %c %s", "font-weight: bold", evt.inputType, "font-weight: normal", evt.data)
+
+            evt.preventDefault()
+        })
+
+        htmlInput.addEventListener("compositionstart", {evt ->
+            evt as CompositionEvent
+            console.log(evt.type, evt.timeStamp, evt.data)
+            if (lastKeydownWasProcessed) {
+                composeSender.sendEditCommand(DeleteSurroundingTextInCodePointsCommand(1, 0))
+            }
+
+            editState  = EditState.CompositeDialogueMode
+        })
+
+        htmlInput.addEventListener("compositionupdate", {evt ->
+            evt as CompositionEvent
+            console.log(evt.type, evt.timeStamp, evt.data)
+
+            composeSender.sendEditCommand(SetComposingTextCommand(evt.data, 1))
+            evt.preventDefault()
+        })
+
+        htmlInput.addEventListener("compositionend", {evt ->
+            evt as CompositionEvent
+            console.log(evt.type, evt.timeStamp, evt.data)
+            evt.preventDefault()
+
+            editState = EditState.WaitingComposeActivity
+            composeSender.sendEditCommand(CommitTextCommand(evt.data, 1))
+        })
+    }
+
+    override fun updateState(textFieldValue: TextFieldValue) {
+        if (editState != EditState.WaitingComposeActivity) return
+
+        println("updateState $editState ${textFieldValue.text}")
+
+        htmlInput.value = textFieldValue.text
+        htmlInput.setSelectionRange(textFieldValue.selection.start, textFieldValue.selection.end)
+
+        editState = EditState.Default
+    }
+}
+
+
+private sealed interface EditState {
+    data object Default : EditState
+    data object WaitingComposeActivity : EditState
+    data object CompositeDialogueMode: EditState
+    data object AccentDialogueMode: EditState
+}
