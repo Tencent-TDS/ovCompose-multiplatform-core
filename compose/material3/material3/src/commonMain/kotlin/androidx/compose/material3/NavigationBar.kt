xiaocainiaooo/room3

Remove padding for NavigationBarItem label to align with the spec.
Bug: b/330198163
Test: N/A
Change-Id: I4d78881f066df576668581bf637c87ed74028199

compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/NavigationBar.kt
@@ -31,7 +31,6 @@ import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.only
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
@@ -565,11 +564,9 @@ private fun NavigationBarItemLayout(
 
             if (label != null) {
                 Box(
-                    Modifier.layoutId(LabelLayoutIdTag)
-                        .graphicsLayer {
-                            alpha = if (alwaysShowLabel) 1f else alphaAnimationProgress()
-                        }
-                        .padding(horizontal = NavigationBarItemHorizontalPadding / 2)
+                    Modifier.layoutId(LabelLayoutIdTag).graphicsLayer {
+                        alpha = if (alwaysShowLabel) 1f else alphaAnimationProgress()
+                    }
                 ) {
                     label()
                 }

Tencent-TDS/ovCompose-multiplatform-core

(cherry picked from https://android-review.googlesource.com/q/commit:73709f093830361c7a7dfe9fb0092be9ee1bf6cc)
(cherry picked from https://android-review.googlesource.com/q/commit:752734f8760fc450b77d0cfb106f51813481e55e)
Merged-In: I4d78881f066df576668581bf637c87ed74028199

compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/NavigationBar.kt
@@ -32,7 +32,6 @@ import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.only
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
@@ -523,9 +522,9 @@ private fun NavigationBarItemLayout(
 
         if (label != null) {
             Box(
-                Modifier.layoutId(LabelLayoutIdTag)
-                    .graphicsLayer { alpha = if (alwaysShowLabel) 1f else animationProgress() }
-                    .padding(horizontal = NavigationBarItemHorizontalPadding / 2)
+                Modifier.layoutId(LabelLayoutIdTag).graphicsLayer {
+                    alpha = if (alwaysShowLabel) 1f else animationProgress()
+                }
             ) {
                 label()
             }